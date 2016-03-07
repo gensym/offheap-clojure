da03aaf3c0a1d5ed7888e3e71c282cb302dfc4cf
David Altenburg <daltenburg@drwholdings.com>
2016-03-06 21:10:51 -0600

    Upgraded to Clojure 8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.5"]
                  [clj-time "0.8.0"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
